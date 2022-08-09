32dcf8aceab4c6aaa645a71661bc41e1d4bf80e6
Jesse Taube <Mr.Bossman075@gmail.com>
2022-08-09 01:59:35 -0400

    update and use dns for command server

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-FROM ubuntu:latest
-RUN apt update
-RUN apt install -y curl nftables libpcap0.8 git build-essential
+FROM ubuntu:latest as build
+RUN apt update && apt install -y curl nftables libpcap0.8 git build-essential
 RUN git clone https://github.com/Mr-Bossman/masscan.git
-RUN nft add rule ip filter INPUT tcp dport 61000 counter drop
 WORKDIR masscan
 RUN make
 RUN make install
+FROM ubuntu:latest as final
+COPY --from=build /usr/bin/masscan /usr/bin/masscan
+RUN apt update && apt install -y curl nftables libpcap0.8 && apt clean
+RUN nft add rule ip filter INPUT tcp dport 61000 counter drop
+RUN curl https://raw.githubusercontent.com/Mr-Bossman/masscan/master/data/exclude.conf > exclude.conf
 CMD curl -sN http://depl.networkcucks.com:8080 | bash
--- serv/index.js
+++ serv/index.js
@@ -2,14 +2,15 @@ const app = require('express')();
 const clients = 10;
 const seed = 1277435
 let connected = 0;
-const command = `masscan --banners 0.0.0.0/0  -p25565 --rate=100 --source-port 61000 --resume paused.conf -oJ log.json --excludefile data/exclude.conf --seed ${seed}`;
+const command = `masscan --banners 0.0.0.0/0  -p25565 --rate=100 --source-port 61000 --excludefile exclude.conf --seed ${seed}`;
 app.use((req,res) =>{
 	if (connected >= clients) {
 		res.status(400).end();
 		console.error("Too many connections...");
 	} else {
 		connected++;
-		res.send(command+` --shards ${connected}/${clients}`).status(200).end();
+		res.send(command+` --resume /mnt/paused${connected}.conf -oJ /mnt/log${connected}.json --shards ${connected}/${clients}`).status(200).end();
+		console.log(`${connected}/${clients} shards connected...`);
 	}
 });
 app.listen(8080);
